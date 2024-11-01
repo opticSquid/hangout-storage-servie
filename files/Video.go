c69ff8d4ebd7f66dc3693f1af97c189093705393
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-11-01 18:13:07 +0530

    adding the function definitions of processing media

---
+++ files/Video.go
@@ -0,0 +1,14 @@
+package files
+
+import (
+	"hangout.com/core/storage-service/config"
+	"hangout.com/core/storage-service/logger"
+)
+
+type video struct {
+	filename string
+}
+
+func (v video) processMedia(cfg *config.Config, log logger.Log) {
+
+}
