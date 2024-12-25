6b9bddbf871966a34fea22aa100fe23483042fe9
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-12-25 20:15:57 +0530

    extracting processing in separate functions

--- files/Video.go
+++ files/Video.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"hangout.com/core/storage-service/config"
+	"hangout.com/core/storage-service/files/h264"
 	"hangout.com/core/storage-service/logger"
 )
 
@@ -16,87 +17,34 @@ type video struct {
 func (v *video) processMedia(cfg *config.Config, log logger.Log) error {
 	splittedFilename := strings.Split(v.filename, ".")
 	inputFile := cfg.Hangout.Media.UploadPath + "/" + v.filename
-	outputFile := cfg.Hangout.Media.ProcessedPath + "/" + splittedFilename[0]
+	outputFile := cfg.Hangout.Media.ProcessedPath + "/" + splittedFilename[0] + "/" + splittedFilename[0]
 	var err error
+	os.Mkdir(cfg.Hangout.Media.ProcessedPath+"/"+splittedFilename[0], 0755)
 	err = processH264(inputFile, outputFile, log)
 	if err != nil {
 		log.Error("error in h264 pipeline", "error", err.Error())
 	}
-	err = processH265(inputFile, outputFile, log)
-	if err != nil {
-		log.Error("error in h265 pipeline", "error", err.Error())
-	}
+	// err = processH265(inputFile, outputFile, log)
+	// if err != nil {
+	// 	log.Error("error in h265 pipeline", "error", err.Error())
+	// }
 	// deleting original uploaded file
-	err = os.Remove(inputFile)
-	if err != nil {
-		log.Error("error in deleting original file", "error", err.Error())
-		return err
-	} else {
-		log.Debug("deleted original file")
-	}
+	// err = os.Remove(inputFile)
+	// if err != nil {
+	// 	log.Error("error in deleting original file", "error", err.Error())
+	// 	return err
+	// } else {
+	// 	log.Debug("deleted original file")
+	// }
 	return err
 }
 
 // generte h264 (crf mode and 2 pass mode) encoded versions of the uploaded file in 360p, 720p, 1080p
 func processH264(inputFile string, outputFile string, log logger.Log) error {
 	log.Info("h264 pipeline", "status", "starting")
-	var cmd *exec.Cmd
-	var err error
 
-	log.Debug("pipeline status status", "encoder", "h264", "method", "crf", "status", "starting")
-	// creating 360p, 720p, 1080p using h264 crf mode
-	cmd = exec.Command("ffmpeg", "-i", inputFile,
-		"-c:v", "libx264", "-crf", "23", "-maxrate", "500k", "-bufsize", "1M", "-vf", "scale=-2:360", outputFile+"_h264_360p.mp4",
-		"-c:v", "libx264", "-crf", "23", "-maxrate", "1M", "-bufsize", "3M", "-vf", "scale=-2:720", outputFile+"_h264_720p.mp4",
-	)
-	_, err = cmd.Output()
-	if err != nil {
-		log.Error("error in processing h264 crf workflow", "error", err.Error())
-	} else {
-		log.Debug("pipeline status", "encoder", "h264", "method", "crf", "status", "finished")
-	}
-
-	// creating 1080p using h264 2 pass mode
-	log.Debug("pipeline status", "encoder", "h264", "method", "2 pass", "status", "starting")
-	// doing 1st pass
-	cmd = exec.Command("ffmpeg", "-i", inputFile,
-		"-c:v", "libx264", "-pass", "1", "-passlogfile", outputFile, "-b:v", "2M", "-fps_mode", "cfr", "-vf", "scale=-2:1080", "-f", "null", "/dev/null",
-	)
-	_, err = cmd.Output()
-	if err != nil {
-		log.Error("error in processing h264 2 pass workflow, error in 1st pass", "current resolution", "1080p", "error", err.Error())
-	} else {
-		log.Debug("pipeline status", "encoder", "h264", "method", "2 pass", "current pass", 1, "current resolution", "1080p", "status", "finished")
-	}
-	// doing 2nd pass
-	// creating 1080p video in 2nd pass out of 1st pass log and mbtree files
-	cmd = exec.Command("ffmpeg", "-i", inputFile,
-		"-c:v", "libx264", "-pass:", "2", "-passlogfile", outputFile, "-b:v", "2M", "-fps_mode", "cfr", "-vf", "scale=-2:1080", outputFile+"_h264_1080p.mp4",
-	)
-	_, err = cmd.Output()
-	if err != nil {
-		log.Error("error in processing h264 2 pass workflow, error in 2nd pass", "current resolution", "1080p", "error", err.Error())
-	} else {
-		log.Debug("pipeline status", "encoder", "h264", "method", "2 pass", "current pass", 2, "current resolution", "1080p", "status", "finished")
-	}
-
-	// deleting ffmpeg generated log file
-	err = os.Remove(outputFile + "-0.log")
-	if err != nil {
-		log.Error("error in deleting ffmpeg  h264 log file", "current resolution", "1080p", "error", err.Error())
-		return err
-	} else {
-		log.Debug("deleted ffmpeg h264 log file", "current resolution", "1080p")
-	}
-	// deleting ffmpeg generated mbtree file
-	err = os.Remove(outputFile + "-0.log.mbtree")
-	if err != nil {
-		log.Error("error in deleting ffmpeg h264 mbtree file", "current resolution", "1080p", "error", err.Error())
-		return err
-	} else {
-		log.Debug("deleted ffmpeg h264 mbtree file", "current resolution", "1080p")
-	}
-	log.Debug("pipeline status", "encoder", "h264", "method", "2 pass", "status", "finished")
+	h264.ProcessCRFmode(inputFile, outputFile, log)
+	h264.Process2PassMode(inputFile, outputFile, log)
 
 	log.Info("h264 pipeline", "status", "finished")
 	return nil
---
+++ files/h264/process-crf-mode.go
@@ -0,0 +1,25 @@
+package h264
+
+import (
+	"os/exec"
+
+	"hangout.com/core/storage-service/logger"
+)
+
+func ProcessCRFmode(inputFile string, outputFile string, log logger.Log) error {
+	log.Debug("pipeline status status", "encoder", "h264", "method", "crf", "status", "starting")
+	var cmd *exec.Cmd
+	var err error
+	// creating 360p, 720p, 1080p using h264 crf mode
+	cmd = exec.Command("ffmpeg", "-i", inputFile,
+		"-c:v", "libx264", "-crf", "23", "-maxrate", "500k", "-bufsize", "1M", "-vf", "scale=-2:360", outputFile+"_h264_360p.mp4",
+		"-c:v", "libx264", "-crf", "23", "-maxrate", "1M", "-bufsize", "3M", "-vf", "scale=-2:720", outputFile+"_h264_720p.mp4",
+	)
+	_, err = cmd.Output()
+	if err != nil {
+		log.Error("error in processing h264 crf workflow", "error", err.Error())
+	} else {
+		log.Debug("pipeline status", "encoder", "h264", "method", "crf", "status", "finished")
+	}
+	return nil
+}
