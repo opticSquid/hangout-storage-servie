3d8b5888a943f48ef626587c1a24a92ce5d1cd6f
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-12-29 21:48:16 +0530

    reducing segment size to 2 secs

--- files/abr/createPlaylist.go
+++ files/abr/createPlaylist.go
@@ -15,7 +15,7 @@ func CreatePlaylist(outputFilePath string, encoding string, log logger.Log) erro
 	log.Debug("Input", "video file path", videoFile)
 	log.Debug("Input", "audio file path", audioFile)
 	log.Debug("Input", "output file path", outputFilePath)
-	cmd = exec.Command("MP4Box", "-dash", "4000", "-frag", "4000", "-rap", "-segment-name", "segment_$RepresentationID$_", "-fps", "30", videoFile+"640p.mp4#video:id=640p", videoFile+"1280p.mp4#video:id=1280p", videoFile+"1920p.mp4#video:id=1920p", audioFile+"#audio:id=English:role=main", "-out", outputFilePath+".mpd")
+	cmd = exec.Command("MP4Box", "-dash", "2000", "-frag", "2000", "-rap", "-segment-name", "segment_$RepresentationID$_", "-fps", "30", videoFile+"640p.mp4#video:id=640p", videoFile+"1280p.mp4#video:id=1280p", videoFile+"1920p.mp4#video:id=1920p", audioFile+"#audio:id=English:role=main", "-out", outputFilePath+".mpd")
 	_, err = cmd.Output()
 	if err != nil {
 		log.Error("error in processing segmentation and playlist creation", "error", err.Error())
